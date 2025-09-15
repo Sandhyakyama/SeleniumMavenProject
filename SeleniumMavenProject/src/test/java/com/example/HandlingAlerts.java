a38c8ce0a626df5333f29eb041971f7f5207a1bf
Sandhyakyama <89254154+Sandhyakyama@users.noreply.github.com>
2025-09-15 20:30:19 +0530

    Update HandlingAlerts.java

--- SeleniumMavenProject/src/test/java/com/example/HandlingAlerts.java
+++ SeleniumMavenProject/src/test/java/com/example/HandlingAlerts.java
@@ -1,10 +1,48 @@
 package com.example;
 
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
 public class HandlingAlerts extends App{
-	
-	public void setup() {
+	@Test
+	public void AlertHandling() {
+		Setup();
+		//Alert box
+		driver.get("https://www.hyrtutorials.com/p/alertsdemo.html");    
+		driver.findElement(By.id("alertBox")).click();
+		System.out.print(driver.switchTo().alert().getText());
+		driver.switchTo().alert().accept();
+		
+		// confirm box
+			//Alert accept
+		/*driver.findElement(By.id("confirmBox")).click();
+		System.out.print(driver.switchTo().alert().getText());
+		driver.switchTo().alert().accept();
+		System.out.print("I clicked ok");*/
+		
+		//Alert dismiss
+		driver.findElement(By.id("confirmBox")).click();
+		System.out.print(driver.switchTo().alert().getText());
+		driver.switchTo().alert().accept();
+		System.out.print("I clicked cancle");
 		
 		
+		//Prompt box
+		//Alert accept
+		driver.findElement(By.id("promptBox")).click();
+		System.out.print(driver.switchTo().alert().getText());
+		driver.switchTo().alert().sendKeys("Sandhya");
+		driver.switchTo().alert().accept();
+		System.out.print("I clicked Ok");
+		      
+		//Alert dismiss
+		driver.findElement(By.id("promptBox")).click();
+		System.out.print(driver.switchTo().alert().getText());
+		driver.switchTo().alert().sendKeys("Sandhya");
+		driver.switchTo().alert().accept();
+		System.out.print("I clicked Ok");
+		
+
 		
 		
 	}
